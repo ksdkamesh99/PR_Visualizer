6d9ac921ddf668739b2449bb5f882bf4efe03098
imabp <abir.pal899@gmail.com>
2021-02-20 12:01:44 +0530

    Updated Piechart

--- app/src/components/piechart/index.jsx
+++ app/src/components/piechart/index.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Pie } from "react-chartjs-2";
+import {Box} from "@material-ui/core"
 import colorConfig from "./color.config";
 const PieChart = (props) => {
   const { merge,open,close } = props; //count
-  const { merged, opened, closed } = colorConfig; //colors
+  const { merged, opened, closed,container } = colorConfig; //colors
   const data = {
     labels: ["Merged", "Open", "Closed"],
     datasets: [
@@ -16,7 +17,9 @@ const PieChart = (props) => {
     ],
   };
   return <>
-  <Pie data={data}/>
+  <Box style={{background:container, border:'1px solid black',borderRadius:'5px'}}><br/>
+  <Pie style={{margin:'-15px'}} data={data}/><br/>
+  </Box>
   </>;
 };
 
