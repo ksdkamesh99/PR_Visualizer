3d7e4022da4f34c24a5c79f6c07bd64a15b4514c
imabp <abir.pal899@gmail.com>
2021-02-25 23:21:28 +0530

    Lot of Bad Code, but Its working fine

--- app/src/App.js
+++ app/src/App.js
@@ -1,18 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import { PRBox, PieChart } from "./components";
-import { Grid, Container } from "@material-ui/core";
-
+import { Grid, Container, Button } from "@material-ui/core";
+import _ from "lodash";
+import cleanData from "./utils/clean_data";
 const App = (props) => {
-  const { data } = props; // const data = props.data
+  const { data } = props;
+  const [prData,setPrData] =useState(data)
+
+  const displayAll=()=>{
+    setPrData(data)
+  }
+
+  const displayClose=()=>{
+    var closeData = _.filter(data, (el) => {
+      return el.state === "closed" && el.merged_at===null;
+    });
+    setPrData(closeData)
+  }
+
+  const displayMerged=()=>{
+    var mergeData = _.filter(data, (el) => {
+      return el.state=== "closed" && el.merged_at!==null;
+    });
+    setPrData(mergeData)
+  }
+
+
+
+  var cleanResponse;
   return (
     <>
       <Container>
         <Grid container spacing={6}>
           <Grid item container spacing={4} xs={12} sm={12} md={7} lg={7}>
-            {data.map((el) => {
+            <Grid item spacing={4} xs={12} sm={12} md={4} lg={4}>
+              <Button variant="outlined" onClick={()=>{displayClose()}}>View Closed PRs</Button>
+            </Grid>
+            <Grid item spacing={4} xs={12} sm={12} md={4} lg={4}>
+              <Button variant="outlined" onClick={()=>{displayMerged()}} >View Merged PRs</Button>
+            </Grid>
+            <Grid item spacing={4} xs={12} sm={12} md={4} lg={4}>
+              <Button variant="outlined" onClick={()=>{displayAll()}} >All PRs</Button>
+            </Grid>
+          </Grid>
+          <Grid item container spacing={4} xs={12} sm={12} md={7} lg={7}>
+            {
+              
+         prData &&
+            prData.map((el) => {
+              cleanResponse = cleanData(el);
+              // if(cleanResponse.status==="Merged")
+              // setMergeCount(merged+1);
+              // if(cleanResponse.status="Closed")
+              // setCloseCount(closed+1);
+              // if(cleanResponse.status="Open")
+              // setOpenCount(opened+1);
               return (
                 <Grid item xs={12} md={12} sm={12} lg={12}>
-                  <PRBox title={el.title} />
+                  <PRBox
+                    key = {cleanResponse.number}
+                    title={cleanResponse.title}
+                    number={cleanResponse.number}
+                    status={cleanResponse.status}
+                    opened={cleanResponse.opened}
+                    githublink={cleanResponse.github}
+                  />
                 </Grid>
               );
             })}
--- app/src/components/PRBox/index.jsx
+++ app/src/components/PRBox/index.jsx
@@ -1,44 +1,95 @@
-import React from 'react'
-import config from './color.config'
-import {Box, Grid} from "@material-ui/core"
-const PRBox=(props)=>{
-
-    const {title, number, status, opened, comments, link, tags} = props
-    const { BoxColor,
+import React from "react";
+import config from "./color.config";
+import { Box, Grid } from "@material-ui/core";
+const PRBox = (props) => {
+  const { title, number, status, opened, comments, githublink, tags } = props;
+  var {
+    BoxColor,
     StatusColor,
     OpenColor,
     CommentColor,
     linkColor,
-    linkFontColor} = config
-    return(
-        <>
-        <Grid container spacing={1} style={{border:'1px solid black',borderRadius:'5px',padding:"22px",backgroundColor:BoxColor}}>
-            <Grid  item sm={12} xs={12} md={12} lg={12} id ="PRHeading">
-            <span id="PRno">#{number} </span><span>{title}</span>
-            </Grid>
-            <Grid container item sm={12} xs={12} md={12} lg={12}  id="PRstats">
-                <Grid  item xs={12} md={4} lg={4} sm={12} d="status"
-                style={{padding:'10px',backgroundColor:StatusColor}}
-                >
-                    Status: {status}
-                </Grid>
-                <Grid item xs={12} md={4} lg={4} sm={12} id="opened"
-                style={{padding:'10px',backgroundColor:OpenColor}}
-                >
-                    Opened on: {opened}
-                </Grid>
-                <Grid item xs={12} md={4} lg={4} sm={12} id="comments"
-                style={{padding:'10px',backgroundColor:CommentColor}}
-                >
-                    Total Comments: {comments}
-                </Grid>
-            </Grid>
-            <Grid container item sm={4} xs={4} md={4} lg={4} id="link"
-            >
-               <Grid item style={{padding:'10px',backgroundColor:linkColor,color:linkFontColor}}> View on GitHub</Grid>
-            </Grid>
+    linkFontColor,
+  } = config;
+
+  if (status === "Closed") {
+    StatusColor = "red";
+    BoxColor = "#fc7e7c";
+  }
+
+  if (status === "Merged") {
+    StatusColor = "#b19cd9 ";
+    BoxColor = "#b795fc";
+  }
+  return (
+    <>
+      <Grid
+        container
+        spacing={1}
+        style={{
+          border: "1px solid black",
+          borderRadius: "5px",
+          padding: "22px",
+          backgroundColor: BoxColor,
+        }}
+      >
+        <Grid item sm={12} xs={12} md={12} lg={12} id="PRHeading">
+          <span id="PRno">
+            <b>#{number}</b>{" "}
+          </span>
+          <span>{title}</span>
         </Grid>
-        </>
-    )
-}
+        <Grid container item sm={12} xs={12} md={12} lg={12} id="PRstats">
+          <Grid
+            item
+            xs={12}
+            md={4}
+            lg={4}
+            sm={12}
+            d="status"
+            style={{ padding: "10px", backgroundColor: StatusColor }}
+          >
+            Status: {status}
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            md={4}
+            lg={4}
+            sm={12}
+            id="opened"
+            style={{ padding: "10px", backgroundColor: OpenColor }}
+          >
+            Opened on: {opened}
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            md={4}
+            lg={4}
+            sm={12}
+            id="comments"
+            style={{ padding: "10px", backgroundColor: CommentColor }}
+          >
+            Total Comments: {comments}
+          </Grid>
+        </Grid>
+        <Grid container item sm={4} xs={4} md={4} lg={4} id="link">
+          <Grid
+            item
+            style={{
+              padding: "10px",
+              backgroundColor: linkColor,
+              color: linkFontColor,
+            }}
+          >
+            <a href={githublink} style={{ color: linkFontColor }}>
+              View on GitHub
+            </a>
+          </Grid>
+        </Grid>
+      </Grid>
+    </>
+  );
+};
 export default PRBox;
