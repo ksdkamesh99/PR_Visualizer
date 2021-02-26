0b19652ca4cb126c157f7b5ffbee7f4adc86e284
imabp <abir.pal899@gmail.com>
2021-02-26 23:19:18 +0530

    Added Colors and removed Defaults

--- app/src/data/group.js
+++ app/src/data/group.js
@@ -1,15 +1,16 @@
 var group = [
-  {
-    name: "example Group",
-    prList: [
-      {
-        number: "example number",
-        link: "example link",
-      },
-    ],
-  },
-
+  
 
 ];
 
 export default group;
+
+//  {
+//   name: "example Group",
+//   prList: [
+//     {
+//       number: "example number",
+//       link: "example link",
+//     },
+//   ],
+// },
